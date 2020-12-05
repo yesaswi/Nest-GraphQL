8ddd79067ba9bbe6a41d1b330d6c75002be8dd5a
Yesaswi <yesaswi.vadapalli@gmail.com>
2020-12-05 18:59:18 +0530

    GraphQL Configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
